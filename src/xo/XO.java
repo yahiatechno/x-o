490c3e6e8e496c531b77d9b7835fb1379808f055
DESKTOP-EMM43P5\User <yahiasoliman878@gmail.com>
2023-06-01 15:42:55 +0300

    only x side is finished with the approach of mapping

--- src/xo/XO.java
+++ src/xo/XO.java
@@ -1,33 +1,28 @@
 package xo;
 
 import javax.swing.JFrame;
-
 import java.awt.GridLayout;
-
 import javax.swing.JButton;
 
-import javax.swing.ImageIcon;
-import javax.swing.Icon;
-
 public class XO {
+	
+	// creating 9 buttons  
+	static JButton btn1 = new JButton();    
+	static JButton btn2 = new JButton();    
+	static JButton btn3 = new JButton();    
+	static JButton btn4 = new JButton();
+	static JButton btn5 = new JButton();    
+	static JButton btn6 = new JButton();
+	static JButton btn7 = new JButton();    
+	static JButton btn8 = new JButton();
+	static JButton btn9 = new JButton();
+
 
 	public static void main(String[] args) {
-		JFrame frame01 = new JFrame();    
-		  
-		ImageIcon test = new ImageIcon("C://Users/User/eclipse-workspace/xo/src/xo/x.png");
+		JFrame frame01 = new JFrame();    	
 		
-		// creating 9 buttons  
-		JButton btn1 = new JButton();    
-		JButton btn2 = new JButton();    
-		JButton btn3 = new JButton();    
-		JButton btn4 = new JButton();
-		JButton btn5 = new JButton();    
-		JButton btn6 = new JButton();
-		JButton btn7 = new JButton();    
-		JButton btn8 = new JButton();
-		JButton btn9 = new JButton();
-		
-		btn1.setIcon(test);
+		addListeners();
+		setButtonNames();
 
 		frame01.add(btn1);
 		frame01.add(btn2);
@@ -40,10 +35,37 @@ public class XO {
 		frame01.add(btn9);
 		
 	    frame01.setLayout(new GridLayout(3,3));    
+	   
 	    frame01.setSize(600,600);    
 	    frame01.setVisible(true); 
 		
-
 	}//end main
-
+	
+	public static void addListeners() {
+		
+		listener_1 listenerOne =  new listener_1();
+		btn1.addActionListener(listenerOne);
+		btn2.addActionListener(listenerOne);
+		btn3.addActionListener(listenerOne);
+		btn4.addActionListener(listenerOne);
+		btn5.addActionListener(listenerOne);
+		btn6.addActionListener(listenerOne);
+		btn7.addActionListener(listenerOne);
+		btn8.addActionListener(listenerOne);
+		btn9.addActionListener(listenerOne);
+		
+	}//end addListeners
+	
+	public static void setButtonNames() {
+		btn1.setName("a");
+		btn2.setName("b");
+		btn3.setName("c");
+		btn4.setName("d");
+		btn5.setName("e");
+		btn6.setName("f");
+		btn7.setName("g");
+		btn8.setName("h");
+		btn9.setName("i");
+	}//end setButtonNames
+	
 }//end class
